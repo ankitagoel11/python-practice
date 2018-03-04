370d3f1b8a0f76e8501fdca3d24692209e060d90
ankitagoel11 <31295113+ankitagoel11@users.noreply.github.com>
2018-03-03 18:51:45 -0800

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+number = int(input("Please typr the number "))
+# check if it is divisible by 3
+if number % 2 == 0:
+    print("this number is even")
+else:
+    print("this number is odd")
+if number % 4 == 0 and number % 2 ==0:
+    print("this number is divisible by 2 and 4")
+else:
+    pass
