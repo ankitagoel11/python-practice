67b7e953e24984cb93fc0bbdb5aa2295b320864e
goelankita11 <goel.ankita22@gmail.com>
2018-04-05 22:20:07 -0700

    python practice

--- netflix.py
+++ netflix.py
@@ -19,11 +19,11 @@ csvreader = csv.reader(csvfile, delimiter=",")
     # Loop through looking for the video
 for row in csvreader:
     if row[0] == video:
-        print(row[0]+ " is rated "+ row[1] + " with a rating of " + row[6])
+        print(row[0]+ " is rated @ "+ row[1] + " with a rating of " + row[6])
 
             # Set variable to confirm we have found the video
         found = True
 
     # If the video is never found, alert the user
     if found == False:
-        print("Sorry about this, we don't seem to have what you are looking for!")
+        print("Sorry about this, we don't seem to have what you are looking for!, please try again")
